da90914b51bf65b5c0ce2ef2c99d40d1d30caf81
Your Name <qingcong2001@gmail.com>
2020-07-27 21:45:14 -0700

    More completed questions

---
+++ 148.sort-list.cs
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=148 lang=csharp
+ *
+ * [148] Sort List
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode SortList(ListNode head) 
+    {
+        
+    }
+}
+// @lc code=end
+
