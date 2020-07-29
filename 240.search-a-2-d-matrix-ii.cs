ed5a6c16797c2066a033acc5071c62a0d44f0ef0
Your Name <qingcong2001@gmail.com>
2020-07-28 21:22:26 -0700

    240 search 2D Matrix II done

--- 240.search-a-2-d-matrix-ii.cs
+++ 240.search-a-2-d-matrix-ii.cs
@@ -5,9 +5,34 @@
  */
 
 // @lc code=start
-public class Solution {
-    public bool SearchMatrix(int[,] matrix, int target) {
-        
+public class Solution 
+{
+    public bool SearchMatrix(int[,] matrix, int target) 
+    {
+        //search to right first it right is end or right is larger than target , move down,if down is larger, move left, 
+        int r = 0, c = 0, rowB = matrix.GetLength(0), colB = matrix.GetLength(1);
+        if (rowB == 0 || colB == 0) return false;
+
+        while (r < rowB && c >= 0)
+        {
+            if (matrix[r,c] == target) return true;
+            if (matrix[r,c] < target) 
+            {
+                c++;
+                if (c == colB)
+                {
+                    c--;
+                    r++;
+                }
+            }
+            if (matrix[r,c] > target)
+            {
+                c--;
+                if (c >= 0 && matrix[r,c] <  target) r++;
+            }
+        }
+
+        return false;
     }
 }
 // @lc code=end
